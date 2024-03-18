76d8e9dafae97d62683aa954d29a98d5d66fd444
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-03-18 15:50:49 +0530

    Added variables and count usage

--- hello_terraform/network.tf
+++ hello_terraform/network.tf
@@ -5,3 +5,14 @@ resource "aws_vpc" "network" {
   }
 }
 
+resource "aws_subnet" "subnets" {
+  count             = 6
+  vpc_id            = aws_vpc.network.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index)
+  availability_zone = var.subnet_availability_zone[count.index]
+  tags = {
+    Name = var.subnet_names[count.index]
+  }
+
+}
+
--- hello_terraform/variables.tf
+++ hello_terraform/variables.tf
@@ -1,5 +1,19 @@
 variable "vpc_cidr" {
-    type = string
-    default = "10.0.0.0/16"
-    description = "this is vpc cidr range"
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "this is vpc cidr range"
+}
+
+
+variable "subnet_names" {
+  type    = list(string)
+  default = ["web-1", "web-2", "app-1", "app-2", "db-1", "db-2"]
+
+}
+
+
+variable "subnet_availability_zone" {
+  type    = list(string)
+  default = ["ap-south-1a", "ap-south-1b", "ap-south-1a", "ap-south-1b", "ap-south-1a", "ap-south-1b"]
+
 }
