34f82fd325f21741da451c3706b49b6ba852790f
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-04-22 15:57:23 +0530

    Added subnet 1

--- Batch3/terraform/ntier/main.tf
+++ Batch3/terraform/ntier/main.tf
@@ -1,7 +1,26 @@
+# declaring a vpc
 resource "aws_vpc" "network" {
   cidr_block = var.network_cidr
   tags = {
     Name        = "ntier"
     Environment = "dev"
   }
+}
+
+# declaring a public subnet web1
+resource "aws_subnet" "web1" {
+    cidr_block = var.web1_subnet_info.cidr
+    availability_zone = var.web1_subnet_info.az
+    # implicit dependency on the vpc
+    vpc_id = aws_vpc.network.id 
+    tags = {
+        Name = var.web1_subnet_info.name
+        Environment = "dev"
+    }
+
+    # explicit dependency on the vpc
+    depends_on = [ 
+        aws_vpc.network 
+    ]
+  
 }
--- Batch3/terraform/ntier/variables.tf
+++ Batch3/terraform/ntier/variables.tf
@@ -3,3 +3,16 @@ variable "network_cidr" {
   default     = "192.168.0.0/16"
   description = "cidr for a vpc"
 }
+
+variable "web1_subnet_info" {
+    type = object({
+      cidr = string
+      az = string
+      name = string 
+    })
+    default = {
+      cidr = "192.168.0.0/24"
+      az = "us-west-2a"
+      name = "web1"
+    }
+}
