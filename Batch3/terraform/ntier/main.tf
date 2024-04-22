500508eff337d8030a1da5217e4f1f6bc2a8c7c9
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-04-22 16:13:22 +0530

    Added changes

--- Batch3/terraform/ntier/main.tf
+++ Batch3/terraform/ntier/main.tf
@@ -7,76 +7,16 @@ resource "aws_vpc" "network" {
   }
 }
 
-# declaring a public subnet web1
-resource "aws_subnet" "web1" {
-  cidr_block        = var.web1_subnet_info.cidr
-  availability_zone = var.web1_subnet_info.az
-  # implicit dependency on the vpc
-  vpc_id = aws_vpc.network.id
+# Create subnets
+resource "aws_subnet" "subnets" {
+  count             = length(var.subnets_config)
+  vpc_id            = aws_vpc.network.id
+  cidr_block        = var.subnets_config[count.index].cidr
+  availability_zone = var.subnets_config[count.index].az
   tags = {
-    Name        = var.web1_subnet_info.name
+    Name        = var.subnets_config[count.index].name
     Environment = "dev"
   }
 
-  # explicit dependency on the vpc
-  depends_on = [
-    aws_vpc.network
-  ]
-
-}
-
-
-# declaring a public subnet web2
-resource "aws_subnet" "web2" {
-  cidr_block        = var.web2_subnet_info.cidr
-  availability_zone = var.web2_subnet_info.az
-  # implicit dependency on the vpc
-  vpc_id = aws_vpc.network.id
-  tags = {
-    Name        = var.web2_subnet_info.name
-    Environment = "dev"
-  }
-
-  # explicit dependency on the vpc
-  depends_on = [
-    aws_vpc.network
-  ]
-
-}
-
-# declaring a public subnet db1
-resource "aws_subnet" "db1" {
-  cidr_block        = var.db1_subnet_info.cidr
-  availability_zone = var.db1_subnet_info.az
-  # implicit dependency on the vpc
-  vpc_id = aws_vpc.network.id
-  tags = {
-    Name        = var.db1_subnet_info.name
-    Environment = "dev"
-  }
-
-  # explicit dependency on the vpc
-  depends_on = [
-    aws_vpc.network
-  ]
-
-}
-
-
-# declaring a public subnet web2
-resource "aws_subnet" "db2" {
-  cidr_block        = var.db2_subnet_info.cidr
-  availability_zone = var.db2_subnet_info.az
-  # implicit dependency on the vpc
-  vpc_id = aws_vpc.network.id
-  tags = {
-    Name        = var.db2_subnet_info.name
-    Environment = "dev"
-  }
-
-  # explicit dependency on the vpc
-  depends_on = [
-    aws_vpc.network
-  ]
-
+  depends_on = [aws_vpc.network]
 }
