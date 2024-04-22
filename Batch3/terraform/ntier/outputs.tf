a102fd871232292862dbaa196f10230a8f7838eb
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-04-22 17:28:54 +0530

    Added changes

---
+++ Batch3/terraform/ntier/outputs.tf
@@ -0,0 +1,7 @@
+output "webserver-ip" {
+    value = aws_instance.web1.public_ip
+}
+
+output "ssh-command" {
+    value = "ssh -i ${var.public_instance_config.key_name}.pem ubuntu@${aws_instance.web1.public_ip}"
+}
