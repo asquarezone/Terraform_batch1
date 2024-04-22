70028f25738d80e9287dc0d076dfdc1e31fcbf3a
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-04-22 15:37:40 +0530

    Added network

--- Batch3/terraform/ntier/providers.tf
+++ Batch3/terraform/ntier/providers.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-        version = ">= 5.46.0"
-        source = "hashicorp/aws"
+      version = ">= 5.46.0"
+      source  = "hashicorp/aws"
     }
   }
   required_version = ">= 1.7.4"
 }
 
 provider "aws" {
-    region = "us-west-2"
+  region = "us-west-2"
 }
