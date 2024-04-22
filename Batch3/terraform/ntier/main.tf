6d17c52eddfc94af5c3c2a899e7b62c0b8031996
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-04-22 16:01:54 +0530

    Added 3 more subnets

--- Batch3/terraform/ntier/main.tf
+++ Batch3/terraform/ntier/main.tf
@@ -9,18 +9,74 @@ resource "aws_vpc" "network" {
 
 # declaring a public subnet web1
 resource "aws_subnet" "web1" {
-    cidr_block = var.web1_subnet_info.cidr
-    availability_zone = var.web1_subnet_info.az
-    # implicit dependency on the vpc
-    vpc_id = aws_vpc.network.id 
-    tags = {
-        Name = var.web1_subnet_info.name
-        Environment = "dev"
-    }
+  cidr_block        = var.web1_subnet_info.cidr
+  availability_zone = var.web1_subnet_info.az
+  # implicit dependency on the vpc
+  vpc_id = aws_vpc.network.id
+  tags = {
+    Name        = var.web1_subnet_info.name
+    Environment = "dev"
+  }
 
-    # explicit dependency on the vpc
-    depends_on = [ 
-        aws_vpc.network 
-    ]
-  
+  # explicit dependency on the vpc
+  depends_on = [
+    aws_vpc.network
+  ]
+
+}
+
+
+# declaring a public subnet web2
+resource "aws_subnet" "web2" {
+  cidr_block        = var.web2_subnet_info.cidr
+  availability_zone = var.web2_subnet_info.az
+  # implicit dependency on the vpc
+  vpc_id = aws_vpc.network.id
+  tags = {
+    Name        = var.web2_subnet_info.name
+    Environment = "dev"
+  }
+
+  # explicit dependency on the vpc
+  depends_on = [
+    aws_vpc.network
+  ]
+
+}
+
+# declaring a public subnet db1
+resource "aws_subnet" "db1" {
+  cidr_block        = var.db1_subnet_info.cidr
+  availability_zone = var.db1_subnet_info.az
+  # implicit dependency on the vpc
+  vpc_id = aws_vpc.network.id
+  tags = {
+    Name        = var.db1_subnet_info.name
+    Environment = "dev"
+  }
+
+  # explicit dependency on the vpc
+  depends_on = [
+    aws_vpc.network
+  ]
+
+}
+
+
+# declaring a public subnet web2
+resource "aws_subnet" "db2" {
+  cidr_block        = var.db2_subnet_info.cidr
+  availability_zone = var.db2_subnet_info.az
+  # implicit dependency on the vpc
+  vpc_id = aws_vpc.network.id
+  tags = {
+    Name        = var.db2_subnet_info.name
+    Environment = "dev"
+  }
+
+  # explicit dependency on the vpc
+  depends_on = [
+    aws_vpc.network
+  ]
+
 }
--- Batch3/terraform/ntier/variables.tf
+++ Batch3/terraform/ntier/variables.tf
@@ -5,14 +5,53 @@ variable "network_cidr" {
 }
 
 variable "web1_subnet_info" {
-    type = object({
-      cidr = string
-      az = string
-      name = string 
-    })
-    default = {
-      cidr = "192.168.0.0/24"
-      az = "us-west-2a"
-      name = "web1"
-    }
+  type = object({
+    cidr = string
+    az   = string
+    name = string
+  })
+  default = {
+    cidr = "192.168.0.0/24"
+    az   = "us-west-2a"
+    name = "web1"
+  }
+}
+
+variable "web2_subnet_info" {
+  type = object({
+    cidr = string
+    az   = string
+    name = string
+  })
+  default = {
+    cidr = "192.168.1.0/24"
+    az   = "us-west-2b"
+    name = "web2"
+  }
+}
+
+variable "db1_subnet_info" {
+  type = object({
+    cidr = string
+    az   = string
+    name = string
+  })
+  default = {
+    cidr = "192.168.2.0/24"
+    az   = "us-west-2a"
+    name = "db1"
+  }
+}
+
+variable "db2_subnet_info" {
+  type = object({
+    cidr = string
+    az   = string
+    name = string
+  })
+  default = {
+    cidr = "192.168.3.0/24"
+    az   = "us-west-2b"
+    name = "db2"
+  }
 }
