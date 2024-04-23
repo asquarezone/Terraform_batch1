29d65476524d511a78506d500d8aeb093361a4c3
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-04-23 15:03:25 +0530

    Added changes

---
+++ Batch3/terraform/ntier/loadbalancers.tf
@@ -0,0 +1,71 @@
+# create a network load balancer
+resource "aws_lb" "kids" {
+  name               = "kids-lb"
+  internal           = false
+  load_balancer_type = "network"
+  security_groups    = [aws_security_group.web.id]
+  subnets            = [for subnet in aws_subnet.public_subnets : subnet.id]
+  tags = {
+    Name        = "kids-lb"
+    Environment = "dev"
+  }
+  depends_on = [
+    aws_instance.web1,
+    aws_instance.web2
+  ]
+}
+
+resource "aws_lb_target_group" "kids" {
+  name     = "kids-lb-tg"
+  port     = 80
+  protocol = "TCP"
+  vpc_id   = aws_vpc.network.id
+  depends_on = [
+    aws_instance.web1,
+    aws_instance.web2
+  ]
+}
+
+resource "aws_lb_listener" "kids" {
+  load_balancer_arn = aws_lb.kids.arn
+  port              = 80
+  protocol          = "TCP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.kids.arn
+  }
+  depends_on = [
+    aws_instance.web1,
+    aws_instance.web2,
+    aws_lb.kids,
+    aws_lb_target_group.kids
+  ]
+}
+
+
+resource "aws_lb_target_group_attachment" "web1" {
+
+  target_group_arn = aws_lb_target_group.kids.arn
+  target_id        = aws_instance.web1.id
+  port             = 80
+  depends_on = [
+    aws_instance.web1,
+    aws_instance.web2,
+    aws_lb.kids,
+    aws_lb_target_group.kids
+  ]
+}
+
+
+resource "aws_lb_target_group_attachment" "web2" {
+
+  target_group_arn = aws_lb_target_group.kids.arn
+  target_id        = aws_instance.web2.id
+  port             = 80
+  depends_on = [
+    aws_instance.web1,
+    aws_instance.web2,
+    aws_lb.kids,
+    aws_lb_target_group.kids
+  ]
+}
--- Batch3/terraform/ntier/outputs.tf
+++ Batch3/terraform/ntier/outputs.tf
@@ -1,23 +1,4 @@
-output "webserver1-ip" {
-  value = aws_instance.web1.public_ip
-}
 
-output "web1-ssh-command" {
-  value = "ssh -i ${var.web1_instance_config.key_name}.pem ubuntu@${aws_instance.web1.public_ip}"
-}
-
-output "webserver2-ip" {
-  value = aws_instance.web2.public_ip
-}
-
-output "web2-ssh-command" {
-  value = "ssh -i ${var.web2_instance_config.key_name}.pem ubuntu@${aws_instance.web2.public_ip}"
-}
-
-output "webserver1-url" {
-  value = "http://${aws_instance.web1.public_ip}/kids"
-}
-
-output "webserver2-url" {
-  value = "http://${aws_instance.web2.public_ip}/kids"
+output "weburl" {
+    value = "http://${aws_lb.kids.dns_name}/kids"
 }
